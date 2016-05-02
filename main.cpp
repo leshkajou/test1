753c28030e09f2e2d21694fcac3840db44139b30
Алексей Куприянов < >
2016-05-02 18:12:42 +0300

    Box

--- main.cpp
+++ main.cpp
@@ -72,7 +72,7 @@ public:
     void header() {
         WContainerWidget* header = new WContainerWidget(root());
         header->setId("header");
-        header->addWidget(new WText("<h3>" + appName + "</h3>"));
+        header->addWidget(new WText("<h1>" + align="centre" + appName + "</h1>"));
     }
 
     void sidebar() {
@@ -98,7 +98,7 @@ public:
 
 		WTable *table = new Wt::WTable();
 		table->setHeaderCount(1);
-		table->setWidth(Wt::WLength("100%"));//
+		table->setWidth(Wt::WLength("100%"));
 
 		table->elementAt(0, 0)->addWidget(new Wt::WText("#"));
 		table->elementAt(0, 1)->addWidget(new Wt::WText("First Name"));
