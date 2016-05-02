b04471c0998a1f3d58edbf57f3834dea28d06f76
Алексей Куприянов < >
2016-05-02 12:39:27 +0300

    Box

--- main.cpp
+++ main.cpp
@@ -69,22 +69,74 @@ public:
         t->setInternalPathEncoding(true);
         content()->addWidget(t);
 		
-
 		
 
 		Wt::WContainerWidget *container = new Wt::WContainerWidget();
-		container->setStyleClass("yellow-box");
 
-		Wt::WHBoxLayout *hbox = new Wt::WHBoxLayout();
-		container->setLayout(hbox);
+		// Create a navigation bar with a link to a web page.
+		Wt::WNavigationBar *navigation = new Wt::WNavigationBar(container);
+		navigation->setTitle("Corpy Inc.",
+		     "http://www.google.com/search?q=corpy+inc");
+		navigation->setResponsive(true);
 
-		Wt::WText *item = new Wt::WText("Item 1");
-		item->setStyleClass("green-box");
-		hbox->addWidget(item, 1);
-  
-		item = new Wt::WText("Item 2");
-		item->setStyleClass("blue-box");
-		hbox->addWidget(item);
+		Wt::WStackedWidget *contentsStack = new Wt::WStackedWidget(container);
+		contentsStack->addStyleClass("contents");
+
+		// Setup a Left-aligned menu.
+		Wt::WMenu *leftMenu = new Wt::WMenu(contentsStack, container);
+		navigation->addMenu(leftMenu);
+
+		Wt::WText *searchResult = new Wt::WText("Buy or Sell... Bye!");
+
+		leftMenu->addItem("Home", new Wt::WText("There is no better place!"));
+		leftMenu->addItem("Layout", new Wt::WText("Layout contents")) //
+		leftMenu->setLink(Wt::WLink(Wt::WLink::InternalPath, "/layout"));
+		leftMenu->addItem("Sales", searchResult);
+
+		// Setup a Right-aligned menu.
+		Wt::WMenu *rightMenu = new Wt::WMenu();
+		navigation->addMenu(rightMenu, Wt::AlignRight);
+
+		// Create a popup submenu for the Help menu.
+		Wt::WPopupMenu *popup = new Wt::WPopupMenu();
+		popup->addItem("Contents");
+		popup->addItem("Index");
+		popup->addSeparator();
+		popup->addItem("About");
+
+		popup->itemSelected().connect(std::bind([=] (Wt::WMenuItem *item) {
+		Wt::WMessageBox *messageBox = new Wt::WMessageBox
+		("Help",
+		Wt::WString::fromUTF8("<p>Showing Help: {1}</p>").arg(item->text()),
+		Wt::Information, Wt::Ok);
+
+		messageBox->buttonClicked().connect(std::bind([=] () {
+		delete messageBox;
+    }));
+
+    messageBox->show();
+}, std::placeholders::_1));
+
+Wt::WMenuItem *item = new Wt::WMenuItem("Help");
+item->setMenu(popup);
+rightMenu->addItem(item);
+
+// Add a Search control.
+Wt::WLineEdit *edit = new Wt::WLineEdit();
+edit->setEmptyText("Enter a search item");
+
+edit->enterPressed().connect(std::bind([=] () {
+    leftMenu->select(2); // is the index of the "Sales"
+    searchResult->setText(Wt::WString("Nothing found for {1}.")
+			  .arg(edit->text()));
+}));
+
+navigation->addSearch(edit, Wt::AlignRight);
+
+container->addWidget(contentsStack);
+		
+
+		
     }
 
     void page1() {
