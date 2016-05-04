e9d02b8983b8777995959902eb05d75d08924c13
user <kupriyanov_95@inbox.ru>
2016-05-04 17:00:01 +0300

    ubuntu

---
+++ build/CMakeFiles/hellowt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellowt.dir/main.cpp.o"
+  "hellowt.pdb"
+  "hellowt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellowt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -8,28 +8,35 @@
 #include <Wt/WTable>
 #include <Wt/WTableCell>
 #include <Wt/WLineEdit>
+#include <Wt/WBreak>
+#include <Wt/WMenu>
+#include <Wt/WStackedWidget>
+#include <Wt/WTextArea>
+#include <Wt/WGroupBox>
+#include <Wt/WText>
+#include <Wt/WVBoxLayout>
 
 
 //HEY!
 using namespace Wt;
 namespace {
-    struct Employee {
-	std::string firstName;
-	std::string lastName;
-	double pay;
+    struct BestBook {
+	std::string bookName;
+	std::string authorName;
+	std::string markBook;
 
-        Employee(const std::string& aFirstName, 
-		 const std::string& aLastName,
-		 double aPay)
-	  : firstName(aFirstName),
-	    lastName(aLastName),
-	    pay(aPay) { }
+        BestBook(const std::string& abook, 
+		 const std::string& aauthor,
+		 const std::string& amark)
+	  : bookName(abook),
+	    authorName(aauthor),
+	    markBook(amark) { }
     };
 
-    Employee employees[] = {
-      Employee("Mark", "Otto", 100),
-      Employee("Jacob", "Thornton", 50),
-      Employee("Larry the Bird", "", 10)
+    BestBook books[] = {
+      BestBook("Ken Kizi ", "One Flew Over the Cuckoo's Nest ", "10"),
+      BestBook("Lev Tolstoy ", "War and peace ", "9.5"),
+      BestBook("Anton Chehov ", "Krizhovnic ", "9")
     };
 }
 
@@ -72,13 +79,49 @@ public:
     void header() {
         WContainerWidget* header = new WContainerWidget(root());
         header->setId("header");
-        header->addWidget(new WText("<h1>" + align="centre" + appName + "</h1>"));
+	header->addStyleClass("centered-example");
+        header->addWidget(new WText("<h1>"  + appName + "</h1>")); 
+	
     }
 
     void sidebar() {
         WContainerWidget* sidebar = new WContainerWidget(root());
         sidebar->setId("sidebar");
-        sidebar->addWidget(new WText("Sidebar Information"));
+        sidebar->addWidget(new WText("Sidebar Menu"));
+//##### SideBarMenu
+	WGroupBox *groupBox = new Wt::WGroupBox();
+	groupBox->addStyleClass("centered-example");
+//
+	WText *authors = new WText("<a href='#/page1'><strong>authors</strong></a><br /><br />");
+	authors->setInternalPathEncoding(true);
+	groupBox->addWidget(authors);
+//
+	WText *types = new WText("<a href='#/page1'><strong>types of books</strong></a><br /><br />");
+	types->setInternalPathEncoding(true);
+	groupBox->addWidget(types);
+//
+	WText *rates = new WText("<a href='#/page1'><strong>rates</strong></a><br /><br />");
+	rates->setInternalPathEncoding(true);
+	groupBox->addWidget(rates);
+//
+	WText *newbook = new WText("<a href='#/page1'><strong>add new book</strong></a><br /><br />");
+	newbook->setInternalPathEncoding(true);
+	groupBox->addWidget(newbook);
+//
+	WText *series = new WText("<a href='#/page1'><strong>series and cycles</strong></a><br /><br />");
+	series->setInternalPathEncoding(true);
+	groupBox->addWidget(series);
+//
+	WText *reviews = new WText("<a href='#/page1'><strong>wait for reviews</strong></a><br /><br />");
+	reviews->setInternalPathEncoding(true);
+	groupBox->addWidget(reviews);
+//
+	WText *newauthor = new WText("<a href='#/page1'><strong>add new author</strong></a><br /><br />");
+	newauthor->setInternalPathEncoding(true);
+	groupBox->addWidget(newauthor);
+//
+	sidebar->addWidget(groupBox);
+// ############### end of sidebar menu
     }
 
     void footer() {
@@ -91,41 +134,36 @@ public:
         WText *t = new WText("<strong>Home</strong> content and a link to <a href='#/page1'>page1</a>");
         t->setInternalPathEncoding(true);
         content()->addWidget(t);
-		
-		
+// ################ Best books
 
+
+	WTable *table = new WTable();
+	table->setWidth(Wt::WLength("100%"));
+	table->setHeaderCount(1);
+
+	table->elementAt(0, 0)->addWidget(new WText("#"));
+	table->elementAt(0, 1)->addWidget(new WText("Book"));
+	table->elementAt(0, 2)->addWidget(new WText("Author"));
+	table->elementAt(0, 3)->addWidget(new WText("Mark"));
 	
 
-		WTable *table = new Wt::WTable();
-		table->setHeaderCount(1);
-		table->setWidth(Wt::WLength("100%"));
-
-		table->elementAt(0, 0)->addWidget(new Wt::WText("#"));
-		table->elementAt(0, 1)->addWidget(new Wt::WText("First Name"));
-		table->elementAt(0, 2)->addWidget(new Wt::WText("Last Name"));
-		table->elementAt(0, 3)->addWidget(new Wt::WText("Pay"));
-
 	for (unsigned i = 0; i < 3; ++i) {
-		Employee& employee = employees[i];
-		int row = i + 1;
+    	BestBook& book = books[i];
+    	int row = i + 1;
 
-		table->elementAt(row, 0)
-			->addWidget(new Wt::WText(Wt::WString::fromUTF8("{1}")
-				  .arg(row)));
-		table->elementAt(row, 1)
-			->addWidget(new Wt::WText(employee.firstName));
-		table->elementAt(row, 2)
-			->addWidget(new Wt::WText(employee.lastName));
-		table->elementAt(row, 3)
-			->addWidget(new Wt::WLineEdit(Wt::WString::fromUTF8("{1}")
-				      .arg(employee.pay)));
+    new WText(WString::fromUTF8("{1}").arg(row),
+		  table->elementAt(row, 0));
+    new WText(book.bookName, table->elementAt(row, 1));
+    new WText(book.authorName, table->elementAt(row, 2));
+    new WText(book.markBook, table->elementAt(row, 3));
+   
 }
 
-		
-		
+table->addStyleClass("table form-inline");
 
-		
-    }
+
+	content()->addWidget(table);	
+ }
 
     void page1() {
         content()->addWidget(new WText("<strong>Home</strong> content and a link to "));
@@ -138,9 +176,10 @@ WApplication* createApplication(const WEnvironment &env) {
 	WApplication* tmp = new ControlExample(env);
 	tmp->styleSheet().addRule("#header", "background-color: #262626; color: #fff; padding: 0.1em 0.5em;");
 	tmp->styleSheet().addRule("#content", "display: inline-block; width: 65%; padding: 0.5em;");
-	tmp->styleSheet().addRule("#sidebar", "display: inline-block; width: 30%; padding: 0.5em;");
+	tmp->styleSheet().addRule("#sidebar", "display: inline-block; width: 30%; color: #141343; padding: 0.1em 0.5em;");
 	tmp->styleSheet().addRule("#footer", "background-color: #262626; color: #fff; padding: 0.5em;");
 	tmp->styleSheet().addRule("BODY", "background-color: #F0F8FF;");
+	
     return tmp;
 }
 
